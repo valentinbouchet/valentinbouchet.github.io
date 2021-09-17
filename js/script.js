45b064e3b29513632ec8b76463bcff4f9709a2e4
valentinbouchet <72011678+valentinbouchet@users.noreply.github.com>
2021-09-17 16:48:06 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function offsetAnchor() {
+    if (location.hash.length !== 0 && (window.innerHeight + window.scrollY) < document.body.scrollHeight) {
+        window.scrollTo(window.scrollX, window.scrollY - 49);
+    }
+}
+  
+$(document).on('click', 'a[href^="#"]', function(event) {
+    window.setTimeout(function() {
+        offsetAnchor();
+    }, 0);
+});
+  
+window.setTimeout(offsetAnchor, 0);
+
+function test() {
+    window.top
+}
