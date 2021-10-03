0c73a876506effcdcf47b1df848633be0fdce025
Valentin Bouchet <valentin.bouchet@epitech.eu>
2021-10-03 19:27:27 +0200

    Portfolio-F: Update responsive with mobile display (menu display the current section, change display of formation section and only display one column in competences)

--- js/script.js
+++ js/script.js
@@ -9,5 +9,33 @@ $(document).on('click', 'a[href^="#"]', function(event) {
         offsetAnchor();
     }, 0);
 });
-  
+
+$(window).bind('scroll', function() {
+    if($(window).scrollTop() >= $('#contact').offset().top - 50) {
+        $('body header nav ul li').hide();
+        $('body header nav ul li:nth-of-type(6)').show();
+        console.log("contact");
+    } else if($(window).scrollTop() >= $('#portfolio').offset().top - 50) {
+        $('body header nav ul li').hide();
+        $('body header nav ul li:nth-of-type(5)').show();
+        console.log("portfolio");
+    } else if($(window).scrollTop() >= $('#formation').offset().top - 50) {
+        $('body header nav ul li').hide();
+        $('body header nav ul li:nth-of-type(4)').show();
+        console.log("formation");
+    } else if($(window).scrollTop() >= $('#competences').offset().top - 50) {
+        $('body header nav ul li').hide();
+        $('body header nav ul li:nth-of-type(3)').show();
+        console.log("competences");
+    } else if($(window).scrollTop() >= $('#aPropos').offset().top - 50) {
+        $('body header nav ul li').hide();
+        $('body header nav ul li:nth-of-type(2)').show();
+        console.log("aPropos");
+    } else {
+        $('body header nav ul li').hide();
+        $('body header nav ul li:nth-of-type(1)').show();
+        console.log("accueil");
+    }
+});
+    
 window.setTimeout(offsetAnchor, 0);
